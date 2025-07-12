bdc8b34176e80b7a1912f469237aa1b9e4f14121
Master21 <mastercrack2112.5@gmail.com>
2025-07-12 14:44:43 -0600

    Update UX

--- template-one/src/App.jsx
+++ template-one/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Services from './Pages/Services/Services';
+import Clients from './Pages/Clients/Clients';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Clients />
     </>
   )
 }
---
+++ template-one/src/Pages/Clients/Clients.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Clients.css';
+
+const Clients = () => {
+    return(
+        <div className="Clients-style">
+            <h1>Clients</h1>
+            <p>que? este es un texto de prueba</p>
+            <p>no me se el idioma lorem impsi dolor</p>
+        </div>
+    )
+}
+
+export default Clients;
